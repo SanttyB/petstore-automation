fe91ff9802718ad186fb4841b5c48359b3ae1015
Danilo Barbosa <dansantty@gmail.com>
2021-07-08 14:31:37 -0300

    action

--- src/test/java/qa/io/mzowysky/automation/petstore/actions/StoreAction.java
+++ src/test/java/qa/io/mzowysky/automation/petstore/actions/StoreAction.java
@@ -1,5 +1,59 @@
 package qa.io.mzowysky.automation.petstore.actions;
 
-public class StoreAction {
+import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ValidatableResponse;
+import qa.io.mzowysky.automation.petstore.config.EnvConfig;
+
+@Component
+public class StoreAction {
+	
+	private ValidatableResponse request;
+	private final String path="store/";
+	
+	@Autowired
+	private EnvConfig envConfig;
+
+	public ValidatableResponse inventoryStore() throws IOException {
+		envConfig.init();
+		
+		request = RestAssured.given().accept(envConfig.getAccept()).and()
+				.baseUri(envConfig.getBaseurl()).when().get(path + "invetory").then()
+				.statusCode(200);
+		
+		return request;
+	}
+	
+	public ValidatableResponse orderStore(String body) throws IOException {
+		envConfig.init();
+		
+		request = RestAssured.given().contentType(envConfig.getContentType())
+				.accept(envConfig.getAccept()).and().body(body).baseUri(envConfig.getBaseurl())
+				.when().post(path + "order").then().statusCode(200);
+		
+		return request;
+	}
+	
+	public ValidatableResponse findStore() throws IOException {
+		envConfig.init();
+		
+		request = RestAssured.given().accept(envConfig.getAccept()).and().baseUri(envConfig.getBaseurl())
+				.when().get(path).then().statusCode(200);
+		
+		return request;
+	}
+	
+	public ValidatableResponse deleteStore() throws IOException {
+		envConfig.init();
+		
+		request = RestAssured.given().accept(envConfig.getAccept()).and()
+				.baseUri(envConfig.getBaseurl()).when().delete(path).then().statusCode(200);
+		
+		return request;
+	}
 }
+
